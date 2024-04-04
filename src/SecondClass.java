d299075f7ccfe1f4356f2669288c4670c552ce5d
Jlbrown8708 <138274348+Jlbrown8708@users.noreply.github.com>
2024-04-04 14:35:13 -0500

    update

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -1,3 +1,47 @@
-public class SecondClass {
+public class SecondClass {public class SecondClass {
+
+    public static void main (String [] args) {
+        System.out.println("Hello Josh, \n");
+        System.out.print("Hello Jen\n");
+
+        boolean isAlien =false;
+       if (isAlien == true){
+        System.out.print("This is not an ALien");
+       }
+       else System.out.print("This is an Alien");
+    
+
+    int finalScore = 100;
+    if (finalScore <= 99){
+        System.out.print("You were'nt the top score");}
+    if (finalScore==100){
+        System.out.print("You had the highest score");}
+    }
+
+    boolean isCar= false;
+    if(!isCar){ // ! sets it opposite of false which = true
+        System.out.println("this is not a Car");
+    }
+
+          double val1 =20;
+          double val2 = 80;
+          double val3  =((val1+val2)*100);
+          System.out.println(val3);
+          double val4 = val3%40;
+          System.out.println(val4);
+       if (val4==0) {
+        System.out.println("this is true");
+       }
+       else
+       System.out.println("this is not true");
+
+        
+
+}
+}
+
+
+}
+
 
 }
